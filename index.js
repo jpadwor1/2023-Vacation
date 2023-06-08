9a47266924bc96dfd842d79f860b470ca4ef3bef
John Padworski <128070765+jpadwor1@users.noreply.github.com>
2023-06-07 19:11:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ const countdown = setInterval(function(){
 
     if (difference < 0){
         clearInterval(countdown);
-        document.getElementById("#title").innerHTML = "Vacation is here!";
+        document.getElementById("#title").innerHTML = "Have an Amazing Vacation!";
     }
 },1000)
